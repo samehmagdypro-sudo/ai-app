aa72f256da302067ba5a29f19d7e7142cd36ef0b
samehmagdypro-sudo <samehmagdy.pro@gmail.com>
2026-03-30 11:24:12 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = "AIzaSyBpplB6LlK7e6DEUO7gP2kHBilRYFkrSac";
+
+app.post("/generate", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+      {
+        contents: [
+          {
+            parts: [{ text: prompt }]
+          }
+        ]
+      }
+    );
+
+    const result =
+      response.data.candidates[0].content.parts[0].text;
+
+    res.json({ result });
+
+  } catch (error) {
+    res.status(500).json({ error: "AI Error" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is working 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
